e35722449315dfad44424aaa7d94443da3f5c73e
Daan Asscheman <d.asscheman@uu.nl>
2024-05-07 15:56:49 +0200

    User permission issues.

--- docker/backend-entrypoint.sh
+++ docker/backend-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 echo "  ⭐️️️️️⭐️️️️️⭐️️️️️⭐️ VERSIE: 1 "
 # run artisan migrate & seed
-echo "⭐️ Run artisan migrate";
-php artisan migrate --seed
 
-echo "⭐️ generate key";
-php artisan key:generate
+if [ "$APP_ENV" = "local" ]; then
+    echo "⭐️ Run artisan migrate";
+    php artisan migrate --seed
 
+    echo "⭐️ generate key";
+    php artisan key:generate
+fi
 # run apache in foreground
 apache2-foreground

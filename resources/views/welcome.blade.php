ecc66b066fa4b42ca2b136f53d65c554abe5b081
Daan Asscheman <d.asscheman@uu.nl>
2024-06-25 11:51:26 +0200

    Export database credentials to env WIP 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,11 @@
                 </div>
 
 <?php var_dump(env('DB_USERNAME')); ?>
-<?php var_dump(getenv()); ?>
+<br>
+<br>
+<?php var_dump(getenv('LARAVEL_DATABASE_HOST')); ?>
+<br>
+<br>
 <?php var_dump(config('database')); ?>
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">

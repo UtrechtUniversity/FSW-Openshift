9237f3e5ce5fe54cc55bd3a690ea5497b965ff90
Daan Asscheman <d.asscheman@uu.nl>
2024-09-13 14:55:50 +0200

    testing.

--- openshift/openshift-entrypoint.sh
+++ openshift/openshift-entrypoint.sh
@@ -4,8 +4,10 @@ echo "  ⭐️️️️️⭐️️️️️⭐️️️️️⭐️ VERSIE: 1 "
 php artisan config:clear
 
 # run artisan migrate & seed
-echo "⭐️ Run artisan migrate";
+echo "⭐️ Run artisan db show";
 php artisan db:show
+# run artisan migrate & seed
+echo "⭐️ Run artisan migrate";
 php artisan migrate
 
 ##composer install --no-dev --no-scripts
